1bfeed4e92f1ffe1ad58ff8b8893bf51aa0c174e
elmehdihabti <elmehdi.habti@gmail.com>
2023-12-10 18:01:32 +0100

    feat : initier projet

---
+++ src/main/java/com/example/loadfiletoawscloud/LoadFileToAwsCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.loadfiletoawscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadFileToAwsCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadFileToAwsCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loadfiletoawscloud/LoadFileToAwsCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loadfiletoawscloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadFileToAwsCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
